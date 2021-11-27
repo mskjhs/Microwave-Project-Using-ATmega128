37162e5e4be2a3b86eebad8e5983341fe66550ea
mskjhs <91711048+mskjhs@users.noreply.github.com>
2021-11-27 14:31:52 +0900

    Add files via upload

---
+++ module-header-file/extern.h
@@ -0,0 +1,18 @@
+﻿/*
+ * extern.h
+ *
+ * Created: 2021-08-16 오전 9:59:58
+ *  Author: sikwon
+ */ 
+
+
+#ifndef EXTERN_H_
+#define EXTERN_H_
+
+extern volatile uint8_t rxString[100];
+extern volatile uint8_t rxReadyFlag;
+extern volatile int rxindex;
+extern void UART0_ISR_Receive();
+
+
+#endif /* EXTERN_H_ */
---
+++ module-header-file/ultrasonic.h
@@ -0,0 +1,30 @@
+﻿/*
+ * ultrasonic.h
+ *
+ * Created: 2021-09-13 오전 10:52:50
+ *  Author: kccistc
+ */ 
+
+
+#ifndef ULTRASONIC_H_
+#define ULTRASONIC_H_
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define TRUE 1
+#define FALSE 0
+
+#define US_TCN TCNT1     //현재 Timer Counter 1번을 쓴다.
+#define TRIG PG4         // Trigger PIN 
+#define TRIG_DDR  DDRG	 // 방향 설정 출력
+#define TRIG_PORT PORTG
+// echo pin HW interrupt가 지원되는 포트를 잡아서 설정
+// HW external INT4을 잡아서 쓴다. PE4이 INT4를 지원하는 포트
+#define ECHO PE4
+#define ECHO_DDR DDRE
+#define ECHO_PIN PINE
+
+void ultrasonic_init(void);
+void ultrasonic_trigger(void);
+#endif /* ULTRASONIC_H_ */

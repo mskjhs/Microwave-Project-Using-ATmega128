d5c4192c48ba8543158f6f6a251aa5a15214061e
mskjhs <91711048+mskjhs@users.noreply.github.com>
2021-11-27 14:17:13 +0900

    Add files via upload

---
+++ motor_control.h
@@ -0,0 +1,19 @@
+﻿/*
+ * motor_control.h
+ *
+ * Created: 2021-09-14 오후 6:21:22
+ *  Author: kccistc
+ */ 
+
+
+#ifndef MOTOR_CONTROL_H_
+#define MOTOR_CONTROL_H_
+
+
+void pwm_init();
+void stop_fan();
+void forward_direction();
+void backward_direction();
+
+
+#endif /* MOTOR_CONTROL_H_ */
